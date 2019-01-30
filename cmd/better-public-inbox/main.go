b665d29ca1609487f0cf64f0ccfd59c66fb6c230
Maxim Sukharev <max@smacker.ru>
2019-01-30 17:36:01 +0100

    accept path to repo as positional argument

--- cmd/better-public-inbox/main.go
+++ cmd/better-public-inbox/main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/sirupsen/logrus"
-	"github.com/smacker/better-public-inbox"
+	bpi "github.com/smacker/better-public-inbox"
 	"github.com/smacker/better-public-inbox/server"
 )
 
 func main() {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		fmt.Printf("Usage: %s [OPTIONS] path-to-repository\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
 	logrus.SetLevel(logrus.DebugLevel)
 
-	repo := "/Users/smacker/tmp/meta"
-	loader := bpi.NewDirLoader(repo)
+	loader := bpi.NewDirLoader(flag.Arg(0))
 	store, err := bpi.NewMemStore(loader)
 	if err != nil {
 		logrus.Fatal(err)
